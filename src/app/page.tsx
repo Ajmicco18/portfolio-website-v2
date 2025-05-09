3ffbebb5d7dcbc94b07e84d6d70929b2a83a6745
Anthony Micco <anthonymicco2003@gmail.com>
2025-05-09 14:58:50 -0400

    initalized Next.js project

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+  return (
+    <div >
+      <main >
+        <div >
+          <h1>This is going to be my personal portfolio website. </h1>
+        </div>
+      </main>
+      <footer>
+        <p>Designed and Developed by Anthony Micco &copy; 2025</p>
+      </footer>
+    </div>
+  );
+}
