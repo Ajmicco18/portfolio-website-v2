b42be9a268f4693c7211e61d3d1d8a7002ae04fd
Anthony Micco <anthonymicco2003@gmail.com>
2026-01-07 14:14:46 -0500

    updated about me paragraph at top of the page

--- src/app/components/main/AboutMe.tsx
+++ src/app/components/main/AboutMe.tsx
@@ -6,11 +6,10 @@ export const AboutMe = () => {
         <>
             <Box w={{ base: "100%", md: "80%" }} margin={"auto"} padding={8}>
                 <Heading size={"3xl"} fontSize={{ base: "32px", md: "48px" }} color={"white"} padding={3} textDecoration={"underline"} textDecorationColor={"#2D42B9"}>About Me</Heading>
-                <Text as={"p"} padding={3} color={"white"} fontWeight={"bold"}>I am Anthony Micco, I am an undergraduate Computer Science student at Youngstown State University. I am going into my
-                    final semester of school and will graduate with my Bachelor&apos;s Degree in December 2025. After graduation, I intend to continue my education at
-                    Youngstown State and pursue my Data Science Masters. I fell in love with programming and problem solving during my Sophomore year of high school
-                    while taking an AP Computer Science course and from there decided to pursue a career in the field, ideally wanting to specialize in web development,
-                    machine learning, and data visualization.
+                <Text as={"p"} padding={3} color={"white"} fontWeight={"bold"}>I am Anthony Micco and I recently graduated from Youngstown State University with my Bachelor of Science
+                    in Computer Science in December 2025. I am continuing my education by pursuing my Masters of Science in Data Science & Analytics. I fell in love with programming
+                    and problem solving during my Sophomore year of high school while taking an AP Computer Science course and from there decided to pursue a career in the field,
+                    ideally wanting to specialize in web development, machine learning, and data visualization.
                 </Text>
 
             </Box>
